82739561801d173a1e5f82c7b6d8b08ffd96615d
Matthew James Davis <davismj@foursails.co>
2018-11-02 21:33:11 +0900

    feature(edit): Add edit page with two way bindings

--- src/pages/edit.js
+++ src/pages/edit.js
@@ -1,2 +1,13 @@
+import { inject } from 'aurelia-framework';
+import { ContactService } from 'services/contact';
+
+@inject(ContactService)
 export class EditViewModel {
+  constructor(contactService) {
+    this.contactService = contactService;
+  }
+  activate() {
+    return this.contactService.getContactById(1)
+      .then((contact) => this.contact = contact);
+  }
 }
