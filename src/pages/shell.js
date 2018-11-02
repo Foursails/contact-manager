206612100a1f42701e02b4ba548c75959df5fe4f
Matthew James Davis <davismj@foursails.co>
2018-11-02 17:07:26 +0900

    feature(shell): Bind a static contact

--- src/pages/shell.js
+++ src/pages/shell.js
@@ -1,2 +1,9 @@
 export class ShellViewModel {
+  constructor() {
+    this.contact = {
+      name: 'Matthew James Davis',
+      phone: '012-345-6789',
+      email: 'matt.davis@aurelia.io'
+    }
+  }
 }
