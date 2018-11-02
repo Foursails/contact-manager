daf7e59eb99420ed4973efab2447144dc8439729
Matthew James Davis <davismj@foursails.co>
2018-11-02 17:39:22 +0900

    feature(shell): Add contact list

--- src/pages/shell.js
+++ src/pages/shell.js
@@ -2,8 +2,8 @@ export class ShellViewModel {
   constructor() {
     this.contact = {
       name: 'Matthew James Davis',
-      phone: '012-345-6789',
       email: 'matt.davis@aurelia.io'
-    }
+    };
+    this.contacts = [this.contact];
   }
 }
